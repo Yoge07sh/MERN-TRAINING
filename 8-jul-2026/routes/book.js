c29013d1bb2bf0af32f5e31272ea5919a8cf24c2
YOGESH SHARMA <ys742896@gmail.com>
2026-07-09 22:46:25 +0530

    updated code

--- 8-jul-2026/routes/book.js
+++ 8-jul-2026/routes/book.js
@@ -2,27 +2,38 @@ const express = require('express');
 const books = require('../book.json');
 const route = express.Router();
 const fs = require('fs');
-route.get('/books',(req,res)=> {
+route.get('/books', (req, res) => {
     res.json(books);
 })
-route.get('/book/:bookId',(req,res)=> {
+route.get('/book/:bookId', (req, res) => {
     const bookId = req.params.bookId;
     console.log(bookId)
     const book = books.find(book => book.bookId == bookId);
     res.json(book);
 })
-route.post('/add/book',(req,res)=> {
+route.post('/add/book', (req, res) => {
     console.log(req.body);
     books.push(req.body);
-    fs.writeFile('book.json',JSON.stringify(books),(err)=>{
-        if(err) {
+    fs.writeFile('book.json', JSON.stringify(books), (err) => {
+        if (err) {
             res.end("not added");
         } else {
             res.end("added succcesfully ");
         }
     })
 })
-
+route.put('/update/book/:bookId', (req, res) => {
+    const bookId = req.params.bookId;
+    const index = books.findIndex(book => book.bookId == bookId);
+    books[index].bookName = "problem solving";
+    fs.writeFile('books.json', JSON.stringify(books), (err) => {
+        if (err) {
+            res.end("not updated");
+        } else {
+            res.end("updated succcesfully ");
+        }
+    })
+})
 
 
 
