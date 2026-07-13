96719807a15490e19039d6f91db396263d907584
YOGESH SHARMA <ys742896@gmail.com>
2026-07-13 11:26:25 +0530

    updation

--- 8-jul-2026/routes/student.js
+++ 8-jul-2026/routes/student.js
@@ -2,9 +2,9 @@ const express = require('express');
 const students = require('../student.json');
 const route = express.Router();
 const StudentController = require('../controllers/StudentController');
-route.get('/students',StudentController.getStudents);
-route.get('/students/:Rollno',StudentController.getStudentByRollno);
-route.post('/add/student',StudentController.addStudent);
-route.put('/update/student/:Rollno',StudentController.updateStudent);
-route.delete('/delete/student/:Rollno', StudentController.deleteStudent );
+route.get('/students', (req, res) => { StudentController.getStudents(req, res); })
+route.get('/students/:Rollno', (req, res) => { StudentController.getStudentByRollno(req, res) });
+route.post('/add/student', (req, res) => { StudentController.addStudent(req, res) });
+route.put('/update/student/:Rollno', (req, res) => { StudentController.updateStudent(req, res) });
+route.delete('/delete/student/:Rollno', (req, res) => { StudentController.deleteStudent(req, res) });
 module.exports = route;
