f434496ec754c023d07f5066d353281c8c93f888
YOGESH SHARMA <ys742896@gmail.com>
2026-07-21 12:19:23 +0530

    updates

--- 15-jul-2026/controllers/studentController.js
+++ 15-jul-2026/controllers/studentController.js
@@ -16,15 +16,55 @@ const addStudent = async (req, res) => {
 const getStudents = async (req, res) => {
     try {
         const students = await Student.find({});
-        res.render('studentList.ejs', {
+        res.render('studentList', {
             students: students
         });
     } catch (err) {
         console.log(err);
     }
 }
+const getStudentForEdit = async (req, res) => {
+    try {
+        let id = req.params.id;
+        let student = await Student.findOne({ _id: id });
+        res.render('studenteditpage', {
+            student: student
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+const editStudent = async (req, res) => {
+    let id = req.params.id;
+    console.log(req.body);
+    let student = await Student.findOne({ _id: id });
+    student.rollno = req.body.rollno;
+    student.studentName = req.body.studentName;
+    student.fatherName = req.body.fatherName;
+    student.adharCardNo = req.body.adharCardNo;
+    student.mobileNo = req.body.mobileNo;
+    await student.save();
+    const students = await Student.find({});
+    res.render('studentList', { students: students });
+}
+const deleteStudent = async (req, res) => {
+    try {
+        let id = req.params.id;
+        await Student.deleteOne({ _id: id })
+        const students = await Student.find({});
+        res.render('studentList', {
+            students: students
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 module.exports = {
     getHome,
     addStudent,
-    getStudents
+    getStudents,
+    getStudentForEdit,
+    editStudent,
+    deleteStudent
 }
--- 15-jul-2026/routes/studentRoute.js
+++ 15-jul-2026/routes/studentRoute.js
@@ -7,11 +7,22 @@ route.get('/', (req, res) => {
     studentController.getHome(req, res);
 })
 
-route.get('/students', (req, res) => { 
-    studentController.getStudents(req,res);
+route.get('/students', (req, res) => {
+    studentController.getStudents(req, res);
 })
 
 route.post('/add/student', (req, res) => {
     studentController.addStudent(req, res);
+})
+
+route.get('/student/edit/page/:id', (req, res) => {
+    studentController.getStudentForEdit(req, res);
+})
+route.post('/edit/student/:id', (req, res) => {
+    studentController.editStudent(req, res);
+})
+
+route.get('/student/delete/:id', (req, res) => { 
+    studentController.deleteStudent(req,res)
 })
 module.exports = route; 
